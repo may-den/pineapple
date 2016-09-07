b495aa62906d415ae0eed2e668931f23c84a6c2a
Rob Andrews <rob@aphlor.org>
2016-09-07 23:17:28 +0100

    test double stub driver for DB and Common tests

--- test/DB/Driver/TestDriver.php
+++ test/DB/Driver/TestDriver.php
@@ -5,6 +5,10 @@ use Mayden\Pineapple\Util;
 use Mayden\Pineapple\DB;
 use Mayden\Pineapple\DB\Driver\Common;
 
+/**
+ * A null 'test' driver for Pineapple. This serves two purposes: one to act as a scaffold to test scope refactoring,
+ * and to provide a test facility for higher levels of abstraction.
+ */
 class TestDriver extends Common
 {
     var $phptype = 'test';
@@ -50,6 +54,9 @@ class TestDriver extends Common
         1027 => DB::DB_ERROR_VALUE_COUNT_ON_ROW,
     ];
 
+    private $lastQueryType = null;
+    private $sequenceCounter = 1000;
+
     public function __construct()
     {
         parent::__construct();
@@ -68,17 +75,23 @@ class TestDriver extends Common
     public function simpleQuery($query)
     {
         if (preg_match('/^SELECT', $query)) {
+            $this->lastQueryType = 'SELECT';
             return [
-                [
-                    'id' => 1,
-                    'data' => 'test1',
-                ],
-                [
-                    'id' => 2,
-                    'data' => 'test2',
+                'type' => 'resultResource',
+                'results' => [
+                    [
+                        'id' => 1,
+                        'data' => 'test1',
+                    ],
+                    [
+                        'id' => 2,
+                        'data' => 'test2',
+                    ]
                 ]
             ];
         } elseif (preg_match('^INSERT', $query)) {
+            // this may not be correct
+            $this->lastQueryType = 'INSERT';
             return DB::OK;
         } else {
             return $this->myRaiseError();
@@ -92,5 +105,166 @@ class TestDriver extends Common
 
     public function fetchInto($result, &$arr, $fetchmode, $rownum = null)
     {
+        if (!is_null($rownum)) {
+            // hand-picked row
+            if (!isset($result['results'][$rownum])) {
+                // if the row isn't present, don't bother continuing
+                return null;
+            }
+            $row = $result['results'][$rownum];
+        } else {
+            $row = current($result['results']);
+            next($result['results']); // advance ptr, even if we're not using it
+
+            if ($row === false) {
+                return null;
+            }
+        }
+
+        if ($fetchmode & DB::DB_FETCHMODE_ASSOC) {
+            // we're already associative
+            $arr = $row;
+        } else {
+            $arr = array_values($row);
+        }
+
+        return DB::DB_OK;
+    }
+
+    public function freeResult($result)
+    {
+        if (!isset($result['type']) || ($result['type'] != 'resultResource')) {
+            return false;
+        }
+
+        if (isset($result['feignFailure']) && ($result['feignFailure'] === true)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function numCols($result)
+    {
+        if (!isset($result['type']) || ($result['type'] != 'resultResource')) {
+            return $this->myRaiseError();
+        }
+
+        if (isset($result['results'][0])) {
+            return count($result['results'][0]);
+        }
+
+        // honestly the behaviour is undefined for empty result handles. great!
+        return 0;
+    }
+
+    public function numRows($result)
+    {
+        if (!isset($result['type']) || ($result['type'] != 'resultResource')) {
+            return $this->myRaiseError();
+        }
+
+        return count($result['results']);
+    }
+
+    public function autoCommit($onoff = false)
+    {
+        return DB::DB_OK;
+    }
+
+    public function commit()
+    {
+        return DB::DB_OK;
+    }
+
+    public function rollback()
+    {
+        return DB::DB_OK;
+    }
+
+    public function affectedRows()
+    {
+        if ($this->lastQueryType === null) {
+            return $this->myRaiseError();
+        } elseif ($this->lastQueryType === 'INSERT') {
+            return 0;
+        }
+
+        return 2;
+    }
+
+    public function nextId($seq_name, $ondemand = true)
+    {
+        if ($seq_name === 'badsequence') {
+            return $this->myRaiseError();
+        }
+        return $this->sequenceCounter++;
+    }
+
+    public function createSequence($seq_name)
+    {
+        if ($seq_name === 'badsequence') {
+            return $this->myRaiseError();
+        }
+        return DB::DB_OK;
+    }
+
+    public function dropSequence($seq_name)
+    {
+        if ($seq_name === 'badsequence') {
+            return $this->myRaiseError();
+        }
+        return DB::DB_OK;
+    }
+
+    public function quoteIdentifier($str)
+    {
+        return '`' . str_replace('`', '``', $str) . '`';
+    }
+
+    public function escapeSimple($str)
+    {
+        // THE WORST
+        return addslashes($str);
+    }
+
+    private function myRaiseError($errno = null)
+    {
+        return $this->raiseError(
+            12345,
+            null,
+            null,
+            null,
+            'b0rked'
+        );
+    }
+
+    public function errorNative()
+    {
+        return 54321;
+    }
+
+    public function tableInfo($result, $mode = null)
+    {
+        if ($result === null) {
+            return $this->myRaiseError();
+        }
+
+        return [
+            [
+                'table' => 'bonzotable',
+                'name' => 'bonzoname',
+                'type' => 'integer',
+                'len' => '12',
+                'flags' => 'NOT NULL',
+            ],
+            [
+                'table' => 'bonzotable',
+                'name' => 'banjoname',
+                'type' => 'string',
+                'len' => '20',
+                'flags' => '',
+            ]
+        ];
     }
 }
