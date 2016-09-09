e6666248fc5cfb2416eb83e38e4ce86a5e4f08e3
Rob Andrews <rob@aphlor.org>
2016-09-09 01:12:40 +0100

    full coverage for DB\Error (ref issue #1)

--- test/DB/ErrorTest.php
+++ test/DB/ErrorTest.php
@@ -1,8 +1,28 @@
 <?php
 namespace Mayden\Pineapple\Test\DB;
 
+use Mayden\Pineapple\DB;
+use Mayden\Pineapple\DB\Error;
 use PHPUnit\Framework\TestCase;
 
 class ErrorTest extends TestCase
 {
+    public function testConstruct()
+    {
+        $error = new Error();
+        $this->assertInstanceOf(Error::class, $error);
+    }
+
+    public function testConstructWithCode()
+    {
+        $error = new Error(DB::DB_ERROR_NOT_FOUND);
+        $this->assertEquals(DB::DB_ERROR_NOT_FOUND, $error->getCode());
+    }
+
+    public function testConstructWithErrorString()
+    {
+        $error = new Error('i are failed');
+        $this->assertEquals(DB::DB_ERROR, $error->getCode());
+        $this->assertEquals('DB Error: i are failed', $error->getMessage());
+    }
 }
