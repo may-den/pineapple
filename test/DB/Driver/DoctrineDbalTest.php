a0a1a7fef6895d7175ef88e7d7326b4e47a071e7
Rob Andrews <rob.andrews@mayden.co.uk>
2016-09-14 14:37:49 +0100

    start of the dbal driver test

--- test/DB/Driver/DoctrineDbalTest.php
+++ test/DB/Driver/DoctrineDbalTest.php
@@ -1,8 +1,15 @@
 <?php
 namespace Mayden\Pineapple\Test\DB\Driver;
 
+use Mayden\Pineapple\DB;
+use Mayden\Pineapple\DB\Driver\DoctrineDbal;
 use PHPUnit\Framework\TestCase;
 
 class DoctrineDbalTest extends TestCase
 {
+    public function testConstruct()
+    {
+        $dbh = DB::connect(DoctrineDbal::class . '://');
+        $this->assertInstanceOf(DoctrineDbal::class, $dbh);
+    }
 }
