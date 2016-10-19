2ae6b4c6dea903b9bc23e09ee0df9d9df0799f7e
Rob Andrews <rob@aphlor.org>
2016-10-19 22:56:22 +0100

    please tell me nobody uses ms access. please?

--- src/DB.php
+++ src/DB.php
@@ -254,10 +254,6 @@ class DB
      *
      * + mysql, mysqli:  change unique/primary key constraints
      *   DB_ERROR_ALREADY_EXISTS -> DB_ERROR_CONSTRAINT
-     *
-     * + odbc(access):  MS's ODBC driver reports 'no such field' as code
-     *   07001, which means 'too few parameters.'  When this option is on
-     *   that code gets mapped to DB_ERROR_NOSUCHFIELD.
      */
     // @const Convert error messages to a consistent type across DB layers
     const DB_PORTABILITY_ERRORS = 16;
